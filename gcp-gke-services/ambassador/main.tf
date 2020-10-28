ae3b2566df4d22f205c9c1736cf1bf54bcc39707
aetorres <andres.torresduran@gmail.com>
2020-10-28 08:28:33 -0600

    login into the cluster to install ambassador

--- gcp-gke-services/ambassador/main.tf
+++ gcp-gke-services/ambassador/main.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "ambassador-services" {
   provisioner "local-exec" {
     command = <<EOT
+    gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1
     kubectl create clusterrolebinding $CLUSTER_NAME-admin-binding --clusterrole=cluster-admin --user=$(gcloud info --format="value(config.account)")
     kubectl apply -f https://www.getambassador.io/yaml/ambassador/ambassador-crds.yaml
     kubectl apply -f https://www.getambassador.io/yaml/ambassador/ambassador-rbac.yaml
