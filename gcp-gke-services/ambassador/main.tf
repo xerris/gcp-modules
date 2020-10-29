93adece52c46415ed5c4b95f17087e9b727efc66
aetorres <andres.torresduran@gmail.com>
2020-10-29 16:18:56 -0600

    ingress creation

--- gcp-gke-services/ambassador/main.tf
+++ gcp-gke-services/ambassador/main.tf
@@ -6,6 +6,7 @@ resource "null_resource" "ambassador-services" {
     kubectl create clusterrolebinding $CLUSTER_NAME-admin-binding --clusterrole=cluster-admin --user=$(gcloud info --format="value(config.account)")
     kubectl apply -f https://www.getambassador.io/yaml/ambassador/ambassador-crds.yaml
     kubectl apply -f https://www.getambassador.io/yaml/ambassador/ambassador-rbac.yaml
+    kubectl apply -f ambassador-ingress.yaml
     EOT
     environment = {
       CLUSTER_NAME =  var.cluster_name
