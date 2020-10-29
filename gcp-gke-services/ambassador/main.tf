0e0002afd94e904de6c7fea997f75a6dc9ed56bd
aetorres <andres.torresduran@gmail.com>
2020-10-29 13:40:52 -0600

    updating stage env

--- gcp-gke-services/ambassador/main.tf
+++ gcp-gke-services/ambassador/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "ambassador-services" {
   provisioner "local-exec" {
     command = <<EOT
-    gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1
+    gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION
     kubectl cluster-info
     kubectl create clusterrolebinding $CLUSTER_NAME-admin-binding --clusterrole=cluster-admin --user=$(gcloud info --format="value(config.account)")
     kubectl apply -f https://www.getambassador.io/yaml/ambassador/ambassador-crds.yaml
@@ -9,8 +9,10 @@ resource "null_resource" "ambassador-services" {
     EOT
     environment = {
       CLUSTER_NAME =  var.cluster_name
+      REGION = var.location
     }
   }
 }
 
 variable "cluster_name"{}
+variable "location"{}
